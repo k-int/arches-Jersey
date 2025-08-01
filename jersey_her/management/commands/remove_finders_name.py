d0207e4debd7f5f86e2290ef884c42ae6aef7563
Chris <chris.damm@k-int.co.uk>
2025-08-01 16:03:10 +0100

    reformatting files with black (#6)

--- jersey_her/management/commands/remove_finders_name.py
+++ jersey_her/management/commands/remove_finders_name.py
@@ -2,15 +2,15 @@
 from django.core.management.base import BaseCommand, CommandError
 from arches.app.models.tile import Tile
 
-class Command(BaseCommand):
 
-    '''
+class Command(BaseCommand):
+    """
     Remove finders name from all Find resources
-    '''
+    """
 
     def handle(self, *arg, **options):
 
-        tiles = Tile.objects.filter(nodegroup_id = "26243e3f-fd58-11e6-9e3e-026d961c88e6")
+        tiles = Tile.objects.filter(nodegroup_id="26243e3f-fd58-11e6-9e3e-026d961c88e6")
         for tile in tiles:
             key = "26243e43-fd58-11e6-9e3e-026d961c88e6"
             if key in tile.data:

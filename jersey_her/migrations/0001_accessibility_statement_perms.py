8bdb0ffc84b7c7145cd5ef822697247b82a2b251
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-04-20 12:33:05 +0100

    rewrite migration as python code

--- jersey_her/migrations/0001_accessibility_statement_perms.py
+++ jersey_her/migrations/0001_accessibility_statement_perms.py
@@ -1,37 +1,39 @@
 from django.db import migrations
 from django.utils.translation import gettext as _
+from arches.app.utils.permission_backend import assign_perm, remove_perm
+from django.contrib.auth.models import Group
+from arches.app.models.models import Plugin
 
 
 class Migration(migrations.Migration):
 
     initial = True
 
-    # dependencies = [("arches_her", "0001_initial")]
+    dependencies = [
+        ("models", "11499_add_editlog_resourceinstance_idx"),
+    ]
 
-    add_accessibility_statement_perms = """
-        insert into guardian_groupobjectpermission (
-            object_pk,
-            content_type_id,
-            group_id,
-            permission_id)
-        values (
-            '289685d5-4d0a-4dbd-8107-78447cab67b9',
-            51,
-            8,
-            205); 
-    """
+    def add_accessibility_perms(apps, schema_editor):
 
-    remove_accessibility_statement_perms = """
-        delete from guardian_groupobjectpermission where 
-        object_pk = '289685d5-4d0a-4dbd-8107-78447cab67b9' and
-        content_type_id = 51 and
-        group_id = 8 and
-        permission_id = 205;
-    """
+        guest_group = Group.objects.get(name="Guest")
+        accessibility_plugin = Plugin.objects.get(
+            pk="289685d5-4d0a-4dbd-8107-78447cab67b9"
+        )
+
+        assign_perm("view_plugin", guest_group, accessibility_plugin)
+
+    def remove_accessibility_perms(apps, schema_editor):
+
+        guest_group = Group.objects.get(name="Guest")
+        accessibility_plugin = Plugin.objects.get(
+            pk="289685d5-4d0a-4dbd-8107-78447cab67b9"
+        )
+
+        remove_perm("view_plugin", guest_group, accessibility_plugin)
 
     operations = [
-        migrations.RunSQL(
-            add_accessibility_statement_perms,
-            remove_accessibility_statement_perms,
+        migrations.RunPython(
+            add_accessibility_perms,
+            remove_accessibility_perms,
         ),
     ]

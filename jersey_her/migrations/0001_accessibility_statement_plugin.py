d679c4450deecfef8bd58a6c468cc74134570eab
CWDamm-Kint <chris.damm@k-int.co.uk>
2026-07-27 16:44:52 +0100

    refactor into single migration

--- jersey_her/migrations/0001_accessibility_statement_plugin.py
+++ jersey_her/migrations/0001_accessibility_statement_plugin.py
@@ -1,20 +1,15 @@
 from django.db import migrations
 from django.utils.translation import gettext as _
+from arches.app.utils.permission_backend import assign_perm, remove_perm
+from django.contrib.auth.models import Group
 
-from arches.app.models.models import Plugin
 
+def add_accessibility_plugin(apps, schema_editor):
 
-class Migration(migrations.Migration):
+    Plugin = apps.get_model("models", "Plugin")
 
-    initial = True
-
-    dependencies = [
-        ("models", "11499_add_editlog_resourceinstance_idx"),
-    ]
-
-    def add_accessibility_plugin(apps, schema_editor):
-
-        instance = Plugin(
+    if not Plugin.objects.filter(pk="289685d5-4d0a-4dbd-8107-78447cab67b9").exists():
+        Plugin.objects.update_or_create(
             pluginid="289685d5-4d0a-4dbd-8107-78447cab67b9",
             name={"en": "Accessibility Plugin"},
             icon="fa fa-universal-access",
@@ -29,15 +24,32 @@ class Migration(migrations.Migration):
             sortorder=0,
         )
 
-        instance.save()
+    guest_group = Group.objects.get(name="Guest")
 
-    def remove_accessibility_plugin(apps, schema_editor):
+    accessibility_plugin = Plugin.objects.get(pk="289685d5-4d0a-4dbd-8107-78447cab67b9")
 
-        accessibility_plugin = Plugin.objects.get(
-            pk="289685d5-4d0a-4dbd-8107-78447cab67b9"
-        )
+    assign_perm("view_plugin", guest_group, accessibility_plugin)
 
-        accessibility_plugin.delete()
+
+def remove_accessibility_plugin(apps, schema_editor):
+
+    Plugin = apps.get_model("models", "Plugin")
+    guest_group = Group.objects.get(name="Guest")
+
+    accessibility_plugin = Plugin.objects.get(pk="289685d5-4d0a-4dbd-8107-78447cab67b9")
+
+    remove_perm("view_plugin", guest_group, accessibility_plugin)
+
+    accessibility_plugin.delete()
+
+
+class Migration(migrations.Migration):
+
+    initial = True
+
+    dependencies = [
+        ("models", "11499_add_editlog_resourceinstance_idx"),
+    ]
 
     operations = [
         migrations.RunPython(
